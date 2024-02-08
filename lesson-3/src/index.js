fe959f0257fc8736e1efa06474245b0bce6ce173
GennadySht <gennady.sht@gmail.com>
2024-02-08 13:28:19 +0200

    222

--- lesson-3/src/index.js
+++ lesson-3/src/index.js
@@ -1,9 +1,7 @@
 import 'dotenv/config'
 import { app } from "./server.js";
-// const express = require('express');
-// const app = express();
+
 const PORT = process.env.PORT || 3000;
-// const HOSTNAME = 'localhost'; // specify your hostname here
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
--- lesson-3/src/server.js
+++ lesson-3/src/server.js
@@ -1,9 +1,42 @@
 import express from "express";
+import fs from 'fs/promises';
+const fileName = './lesson-3/src/db.json';
+async function init() {
+      try {
+            await fs.access(fileName, fs.constants.F_OK);
+      }
+      catch (error) {
+            await fs.writeFile(fileName, JSON.stringify({ users: [] }));
 
+      }
+
+};
+
+await init();
 const app = express();
+app.use(express.json());
 
-app.get("/test", (req, res) => {
-  res.send("Hello, World!");
+app.post("/users", async (req, res) => {
+
+      try {
+            await fs.writeFile(fileName, JSON.stringify(req.body));
+
+            res.send(`${req.body.users.length} users added successfully.`);
+      } catch (error) {
+            console.error('Error adding user:', error);
+            res.status(500).send('Internal Server Error');
+      }
+});
+
+app.get("/users", async (req, res) => {
+      try {
+            const readingContent = await fs.readFile(fileName, 'utf-8');
+
+            res.json(JSON.parse(readingContent));
+      } catch (error) {
+            console.error('Error getting users:', error);
+            res.status(500).send('Internal Server Error');
+      }
 });
 
 export { app };
